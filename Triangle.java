5395502ce6aaa0cd921057909c3327f9298b40bc
Bullchat <dj1040video@gmail.com>
2025-03-31 22:32:01 -0400

    Create Triangle.java

---
+++ Triangle.java
@@ -0,0 +1,16 @@
+public class Triangle {
+    public static void main(String[] args) {
+        // Print A Triangle
+        System.out.println("        T         ");
+        System.out.println("       TTT        ");
+        System.out.println("      TTTTT       ");
+        System.out.println("     TTTTTTT      ");
+        System.out.println("    TTTTTTTTT     ");
+        System.out.println("   TTTTTTTTTTT    ");
+        System.out.println("   TTTTTTTTTTT    ");
+        System.out.println("   TTTTTTTTTTT    ");
+        System.out.println("   TTTTTTTTTTT    ");
+        System.out.println("   TTTTTTTTTTT    ");
+        System.out.println("   TTTTTTTTTTT    ");
+    }
+}
