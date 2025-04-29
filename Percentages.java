ce5713ef4e9b7e3bf294a921204b0079ccdca176
Bullchat <dj1040video@gmail.com>
2025-04-29 13:39:45 -0400

    Update Percentages.java

--- Percentages.java
+++ Percentages.java
@@ -2,17 +2,18 @@ import java.util.Scanner;
 
 public class Percentages {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a double : ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter another double : ");
+        double num2 = scanner.nextDouble();
 
-        System.out.print("Enter two double values separated by space: ");
-        double a = input.nextDouble();
-        double b = input.nextDouble();
-
-        showPercentage(a, b);
-        showPercentage(b, a);
+        computePercent(num1, num2);
+        computePercent(num2, num1);
     }
 
-    static void showPercentage(double x, double y) {
-        System.out.printf("%.2f is %.2f%% of %.2f%n", x, (x / y) * 100, y);
+    public static void computePercent(double num1, double num2) {
+        double percentage = (num1 / num2) * 100;
+        System.out.println(num1 + " is " + percentage + " percent of " + num2);
     }
 }
